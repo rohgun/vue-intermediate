6ff8890c6768e292dc2fc132cc85e928994f59b8
Minsuh Roh <127650200+rohgun@users.noreply.github.com>
2025-07-09 16:06:08 +0900

    sdfsfsfsfd

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <header>
+            <h1>ToDo it!</h1>
+        </header>
+    </div>
+</template>
+
+
+
+<style scoped>
+h1 {
+    color:#2f3B52;
+    font-weight: 900;
+    margin:2.5rem 0 1.5rem ;
+}
+
+</style>
